b44f1a7bb888f9b0baddf5c1ce555c5c39aaccc3
onernemutlu <oner.nemutlu@gmail.com>
2018-11-30 12:55:08 +0300

    Create index.blade.php

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="jumbotron text-center">
+        <h1>OtizART'a Hoş Geldiniz</h1>
+        <p>KromoSoft / ATILIM ÜNİVERSİTESİ</p>
+        <p>Senior Project of 2018 / 2019 Fall</p>
+        <p>CMPE / SE / ISE 494</p>
+
+        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Giriş Yap</a> <a class="btn btn-success btn-lg" href="/register" role="button">Kayıt Ol</a></p>
+    </div>
+@endsection
