0e91ee1bb07fca6c877356f565787a72de29482b
Oner Nemutlu <oner.nemutlu@gmail.com>
2019-01-15 01:48:07 +0300

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Post;
+use App\Products;
+
 
 class DashboardController extends Controller
 {
@@ -24,8 +27,19 @@ class DashboardController extends Controller
      */
     public function index()
     {
+        if(auth()->user()->hasRole('admin'))
+        {
+            //confirmed olcak
+            $posts = Post::orderBy('created_at', 'asc')->paginate(10);
+            //$posts = Post::where('confirmed' , FALSE)->orderBy('created_at' , 'desc')->paginate(5);
+            $products = Products::orderBy('created_at', 'desc')->paginate(10);
+            //$products = Products::where('confirmed' , FALSE)->orderBy('created_at' , 'desc')->paginate(5);
+            $users = User::orderBy('created_at', 'desc')->paginate(10);
+            return view('dashboard')->with('posts', $posts)->with('products', $products)->with('users', $users);
+        }
+
         $user_id = auth()->user()->id;
         $user = User::find($user_id);
-        return view('dashboard')->with('posts', $user->posts);
+        return view('dashboard')->with('posts', $user->posts)->with('products', $user->products)->with('users', $user->name);
     }
 }
