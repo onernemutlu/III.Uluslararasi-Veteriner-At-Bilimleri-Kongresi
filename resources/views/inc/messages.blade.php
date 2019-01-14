840e6d7413c4d61f6671e706b76875aef1b51e62
Oner Nemutlu <oner.nemutlu@gmail.com>
2019-01-15 02:20:00 +0300

    Update messages.blade.php

--- resources/views/inc/messages.blade.php
+++ resources/views/inc/messages.blade.php
@@ -1,23 +1,32 @@
-@if(count($errors) > 0)
+@if(count($errors)>0)
+    @foreach($errors -> all() as $error)
 
-    @foreach(@$errors->all() as $error)
         <div class="alert alert-danger">
+                <br>
+                <br>
+                <br>
             {{$error}}
         </div>
+
     @endforeach
 @endif
 
 @if(session('success'))
         <div class="alert alert-success">
+            <br>
+            <br>
+            <br>
             {{session('success')}}
         </div>
 
-
 @endif
-@if(session('error'))
+
+@if(session('success'))
         <div class="alert alert-danger">
+                <br>
+                <br>
+                <br>
             {{session('error')}}
         </div>
-
 
 @endif
