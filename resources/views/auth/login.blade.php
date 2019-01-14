853584aa40999add1d25e4fe5fe3dd1f40a0db12
Oner Nemutlu <oner.nemutlu@gmail.com>
2019-01-15 02:15:16 +0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,18 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
+<br>
+<br>
+<br>
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Giriş Yap') }}</div>
+                <center><div class="card-header">{{ __('Giriş Yap') }}</div></center>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Posta Adresiniz') }}</label>
+                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Posta Adresi') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
@@ -26,7 +30,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Şifreniz') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Şifre') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
@@ -54,7 +58,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Giriş Yap') }}
+                                    {{ __('Giriş') }}
                                 </button>
 
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
