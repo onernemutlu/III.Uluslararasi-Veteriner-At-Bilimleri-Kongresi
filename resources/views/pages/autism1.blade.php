6247ec59b71b5f30b6cf67f530f6b28d012c5ea7
Oner Nemutlu <oner.nemutlu@gmail.com>
2019-01-15 02:22:10 +0300

    Create autism1.blade.php

---
+++ resources/views/pages/autism1.blade.php
@@ -0,0 +1,273 @@
+@extends('layouts.app')
+
+@section('content')
+<!DOCTYPE html>
+<html>
+<head>
+
+
+</head>
+
+<style>
+
+
+.footer {
+   position: bottom;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: black;
+   color: yellow;
+   text-align: center;
+}
+
+
+
+</style>
+
+    <br>
+    <br>
+    <br>
+
+    <section class="jumbotron text-center">
+            <h3 class="display-3">Otizmli Çocukların Eğitimi İle İlgili Haklar</h3>
+            <br>
+            <a><img src="https://i.ibb.co/680gJCw/Siragu-autism3.jpg" alt="otizart-LOGO" border="0"></a> 
+            <br>
+            <br>
+
+            
+            <!--<h3 class="display-3">Otiz<font color="red">A</font><font color="orange">R</font><font color="blue">T</font></h3>-->
+            <p class="lead text-muted">Otizmli çocukların eğitimi ve öğretimi için genel ilkeler düzenlenmiş olup, yürürlüğe koyulmuştur. Her yaş grubuna farklı seviyede eğitim uygulanmakla beraber, her bireyin de eğitim hakkı vardır. Otizmli çocukların eğitim hakları 5 ana başlık altında toplanabilir:
+
+                <center>    <li>36-72 aylık çocuklar için okul öncesi eğitim programı</li>
+                   <li>İlköğretim okulu programı</li>
+                   <li>İlköğretim okulu orta düzeyde öğrenme yetersizliği olan çocuklar için eğitim programı</li>
+                    <li>Eğitim Uygulama Okulu eğitim programı</li>
+                    <li>Öğretmenler tarafından hazırlanacak kaynaştırma, birebir ve grup eğitim programı</li>
+                </center>
+            </p>
+            <hr>
+            <h2 class="display-4">Otizmli Bireylerin Eğitim Planı</h2>
+            <br>
+            <img src="https://i.ibb.co/D8xbWyD/b.jpg" alt="b" border="0">
+            <br>
+            <br>
+
+            
+              <!--<h3 class="display-3">Otiz<font color="red">A</font><font color="orange">R</font><font color="blue">T</font></h3>-->
+              <p class="lead text-muted">Otizmli çocukların eğitimi, her bireyin performansı belirlenerek başlar. 
+                  Ayrıca, otizmli çocukların eğitimi bireyselleştirilmiş ve erkenden başlatılmış olmalıdır. Bireyi grup eğitimine hazırlamak ve kaynaşmasını sağlamak yine esas amaçlardan biridir. Yetenek düzeyleri açısından birbirleri ile uyuşan 2 ila 4 otizmli birey ile grup eğitimi yapılır. Sosyal ve akademik beceriler yönünden gelişmiş otizmli bireyler, performanslarına uygun kaynaştırma programlarından yararlandırılır. Kaynaştırma eğitimi uygulaması sürecinde kaynaştırma eğitimi alan otizmli bireye, diğer öğrencilere, sınıf öğretmenine, branş öğretmenine ve/veya ailelere özel eğitim desteği sağlanır.
+
+                    Otizmli bireylerde gözlemlenen sorunlar, davranışlar, eğitimin bir parçası olarak nitelendirilir, 
+                    sorunlu davranışın ortadan kaldırılması için uygun yöntem ve teknikler kullanılır. Eğitim öğretim sırasında ilerleme kaydeden otizmli bireylerin performanslarına uygun olarak belirlenen hedeflere göre eğitim ve öğretime devam edilir. Eğitim öğretimde; ortam, materyal, plân ve yöntemler otizmli bireyi merkez alır. Otizmli bireylerin sosyal gelişimlerinin sağlanmasına yönelik gezi, tören ve benzeri etkinlikler, bir program çerçevesinde hazırlanır ve düzenlenir. Otizmli bireylerin eğitim-öğretim sürecinde bireysel özelliklerine uygun öğrenme/öğretme strateji, yöntem ve tekniklerden yararlanılmasına özen gösterilir.
+                    
+                    Kaynaştırma eğitiminin yapıldığı okulun personeli ile ailelerin ve toplumun özel eğitim konusunda bilgilendirilmeleri için gerekli tedbirler alınır. 
+                    Otizmli bireylere resim, işaret ve benzeri yollarla iletişim becerileri kazandırılır.
+              </p>
+              <hr>
+             
+              
+            <h2 class="display-4">Otizm Özel Eğitimin Amaçları</h2>
+            <br>
+            <img src="https://i.ibb.co/m8jHHfQ/autism-header.jpg" alt="b" border="0">
+            <br>
+            <br>
+
+            
+              <!--<h3 class="display-3">Otiz<font color="red">A</font><font color="orange">R</font><font color="blue">T</font></h3>-->
+              <p class="lead text-muted">
+                    <center>    
+                        <li>Özel eğitim gereksinimi olan çocukların, Anayasamız ve Türk Millî Eğitim Sistemini düzenleyen genel esaslar doğrultusunda eğitimleri gerçekleştirilerek bir iş ve meslek sahibi olarak toplumla bütünleşmelerini sağlamak,</li>
+                        <li>Günlük hayatta kendilerine yetecek kadar temel beceri ve alışkanlıklar kazandırarak bağımsız yaşama gücü vermek,</li>
+                        <li>Özel eğitim gereksinimi olan bireylerde görülen problem davranışları, bilimsel yöntem ve teknikler kullanarak ortadan kaldırmak veya en aza indirgemek,</li>
+                        <li>Özel eğitim gereksinimi olan bireyleri topluma hazırlamak, sosyal yönden gelişimlerini sağlayıcı önlemler almak,</li>
+                        <li>Bireyin yetenekleri ve potansiyeli ölçüsünde eğitim hakkından yararlanmasını sağlamak,</li>
+                        <li> Özel eğitim gereksinimi olan bireylerin maddi ve manevî gereksinimlerini karşılayabileceği ortam ve koşullar hazırlamak,</li>
+                        <li> Özel eğitim gereksinimi olan bireylerin özelliklerinden kaynaklanan özür ve problemlerinin oluşturduğu gelişim engellerini ortadan kaldırmak veya etkisini azaltmak.</li>
+                        
+                     </center>
+              </p>
+              <hr>
+
+               
+            <h2 class="display-4">Kaynaklar</h2>
+            
+            <!--<img src="https://i.ibb.co/m8jHHfQ/autism-header.jpg" alt="b" border="0"> -->
+            <br>
+            <br>
+
+            
+              <!--<h3 class="display-3">Otiz<font color="red">A</font><font color="orange">R</font><font color="blue">T</font></h3>-->
+              <p class="lead text-muted">
+                    <center>    
+                        <li>T.C. Milli Eğitim Bakanlığı / Otistik Çocuklar Eğitim Programı</li>
+                        <li>T.C. Milli Eğitim Bakanlığı / Otistik Çocuklar Eğitim Merkezleri Yönergesi</li>
+                        
+                        
+                     </center>
+              </p>
+              <hr>
+              
+            
+
+
+            <p>
+              <a href="/" class="btn btn-primary my-2">Ana Sayfaya Geri Dön</a>
+              
+            </p>
+          </div>
+        </section>
+
+
+
+        <div class="footer">
+            <br>
+            <center><p>&copy; KromoSoft Company 2018-2019</p></center>
+            <a href="#"><i class='fas fa-angle-up' style='font-size:48px;color:red'></i></a>
+          </div>
+          <footer class="page-footer font-small stylish-color-dark pt-4">
+
+              <!-- Footer Links -->
+              <div class="container text-center text-md-left">
+          
+                <!-- Grid row -->
+                <div class="row">
+          
+                  <!-- Grid column -->
+                  <div class="col-md-4 mx-auto">
+          
+                    <!-- Content -->
+                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4">OtizART & KromoSoft Company</h5>
+                    <p>OtizART sayesinde çocuklarınızın yaptığı ürünleri tüm dünyayla paylaşabilirsiniz.
+
+
+                      </p>
+          
+                  </div>
+                  <!-- Grid column -->
+          
+                  <hr class="clearfix w-100 d-md-none">
+          
+                  <!-- Grid column -->
+                  <div class="col-md-2 mx-auto">
+          
+                    <!-- Links -->
+                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Ürünler</h5>
+          
+                    <ul class="list-unstyled">
+                      <li>
+                        <a href="/products">Ürünleri İncele</a>
+                      </li>
+                      
+                    </ul>
+          
+                  </div>
+                  <!-- Grid column -->
+          
+                  <hr class="clearfix w-100 d-md-none">
+          
+                  <!-- Grid column -->
+                  <div class="col-md-2 mx-auto">
+          
+                    <!-- Links -->
+                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Etkinlikler</h5>
+          
+                    <ul class="list-unstyled">
+                      <li>
+                        <a href="/posts">Etkinlikleri İncele</a>
+                      </li>
+                     
+                    </ul>
+          
+                  </div>
+                  <!-- Grid column -->
+          
+                  <hr class="clearfix w-100 d-md-none">
+          
+                  <!-- Grid column -->
+                  <div class="col-md-2 mx-auto">
+          
+                    <!-- Links -->
+                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4">OtizART</h5>
+          
+                    <ul class="list-unstyled">
+                      <li>
+                        <a href="/contact">İletişim</a>
+                      </li>
+                      <li>
+                        <a href="/help">Yardım & Destek</a>
+                      </li>
+                      <li>
+                        <a href="/help">Kullanım Kılavuzları</a>
+                      </li>
+                      <li>
+                        <a href="/about">OtizART Ekibi</a>
+                      </li>
+                    </ul>
+          
+                  </div>
+                  <!-- Grid column -->
+          
+                </div>
+                <!-- Grid row -->
+          
+              </div>
+              <!-- Footer Links -->
+          
+              <hr>
+          
+              <!-- Call to action -->
+              <ul class="list-unstyled list-inline text-center py-2">
+                <li class="list-inline-item">
+                  <h5 class="mb-1">Ücretsiz Üye Ol</h5>
+                </li>
+                <li class="list-inline-item">
+                  <a href="/choose" class="btn btn-danger btn-rounded">Üye Ol</a>
+                </li>
+              </ul>
+              <!-- Call to action -->
+          
+              <hr>
+          
+              <!-- Social buttons -->
+              <ul class="list-unstyled list-inline text-center">
+                <li class="list-inline-item">
+                  <a class="btn-floating btn-fb mx-1">
+                    <i class="fab fa-facebook-f"> </i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a class="btn-floating btn-tw mx-1">
+                    <i class="fab fa-twitter"> </i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a class="btn-floating btn-gplus mx-1">
+                    <i class="fab fa-google-plus-g"> </i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a class="btn-floating btn-li mx-1">
+                    <i class="fab fa-linkedin-in"> </i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a class="btn-floating btn-dribbble mx-1">
+                    <i class="fab fa-dribbble"> </i>
+                  </a>
+                </li>
+              </ul>
+              <!-- Social buttons -->
+          
+              <!-- Copyright -->
+              <div class="footer-copyright text-center py-3">© Copyright:
+                <a href="/"> OtizART.com </a>
+              </div>
+              <!-- Copyright -->
+          
+          </footer>
+          
+
+
+    @endsection
